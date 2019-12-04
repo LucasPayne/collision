5de46b938672bf24fa5aeab69e01e6c6ecfd8810
LucasPayne <lucaspayyne@gmail.com>
2019-12-04 15:02:21 +1300

    preliminaries to resources

--- lib/resources/resources.c
+++ lib/resources/resources.c
@@ -1,5 +1,13 @@
 /*--------------------------------------------------------------------------------
     Resources, resource management, and resource loading module.
+
+notes:
+There could be a problem with paths on the heap. How do they deallocate? What references
+paths?
+
+References don't start at an ID, they do a path lookup and trigger a load if it isn't there. This gets an ID.
+Usage of the ID must always be preceded by a potential relocation.
+
 --------------------------------------------------------------------------------*/
 #include <stdlib.h>
 #include <string.h>
@@ -14,30 +22,28 @@ typedef struct ResourceID_s {
     uint32_t table_index;
     ResourceUUID uuid;
     ResourceType type;
+    char *path;
 } ResourceID;
 
-typedef struct ResourceHandle_s {
-    ResourceID id;
-    char *path;
-} ResourceHandle;
 
 typedef struct ResourceTableEntry_s {
     ResourceUUID uuid;
     ResourceType type;
+    char *path;
     void *resource;
 } ResourceTableEntry;
 
 #define RESOURCE_TABLE_START_SIZE 1024
 static bool g_initialized_resource_table;
-static Resource **g_resource_table;
+static ResourceTableEntry *g_resource_table;
 static ResourceUUID g_last_resource_uuid = 0;
 
 
-static ResourceID create_resource_id(ResourceType type)
+static ResourceID create_resource_id(ResourceType type, char *path)
 {
     if (!g_initialized_resource_table) {
         g_resource_table_size = RESOURCE_TABLE_START_SIZE;
-        g_resource_table = (Resource **) calloc(1, sizeof(Resource *) * g_resource_table_size);
+        g_resource_table = (ResourceTableEntry *) calloc(1, sizeof(ResourceTableEntry) * g_resource_table_size);
         mem_check(g_resource_table);
         g_initialized_resource_table = true;
     }
@@ -49,24 +55,81 @@ static ResourceID create_resource_id(ResourceType type)
                 new_id.table_index = i;
                 new_id.uuid = ++g_last_resource_uuid;
                 new_id.type = type;
+                new_id.path = path;
                 return new_id;
             }
         }
         uint32_t previous_size = g_resource_table_size;
         g_resource_table_size += RESOURCE_TABLE_START_SIZE; // grow the resource table linearly.
-        g_resource_table = (Resource **) realloc(g_resource_table, sizeof(Resource *) * g_resource_table_size);
+        g_resource_table = (ResourceTableEntry *) realloc(g_resource_table, sizeof(ResourceTableEntry) * g_resource_table_size);
         mem_check(g_resource_table);
-        // Null initialize the new available resource pointers.
-        memset(g_resource_table + previous_size, 0, sizeof(Resource *) * (g_resource_table_size - previous_size));
+        // Null initialize the new available resource table entries.
+        memset(g_resource_table + previous_size, 0, sizeof(ResourceTableEntry) * (g_resource_table_size - previous_size));
     }
 }
-static void *get_resource(ResourceID id)
+
+/* static void *get_resource(ResourceID id) */
+/* { */
+/*     if (g_resource_table[id.table_index] != NULL) { */
+/*         if (g_resource_table[id.table_index].uuid != id.uuid) return NULL; */
+/*         if (g_resource_table[id.table_index].type != id.type) return NULL; */
+/*         return g_resource_table[id.table_index].resource; */
+/*     } */
+/* } */
+
+typedef struct ResourceTreeFork_s {
+    struct ResourceTree_s *next;
+    struct ResourceTree_s *first_child;
+} ResourceTreeFork;
+typedef struct ResourceTree_s {
+    bool is_leaf;
+    union contents {
+        ResourceTreeFork fork;
+        ResourceID resource_id;
+    };
+} ResourceTree;
+
+static void add_resource_path(ResourceID id, char *path)
 {
-    if (g_resource_table[id.table_index] != NULL) {
-        if (g_resource_table[id.table_index].uuid != id.uuid) return NULL;
-        if (g_resource_table[id.table_index].type != id.type) return NULL;
-        return g_resource_table[id.table_index].resource;
+    /* Fills out the resource forest enough to store the id at the leaf of this path branch. */
+    char *drive_end = strchr(path, '/');
+    if (drive_end == NULL) {
+        fprintf(stderr, ERROR_ALERT "Invalid resource path \"%s\".\n", path);
+        exit(EXIT_FAILURE);
     }
+    drive_end[0] = '\0';
+    for (int i = 0; i < g_num_resource_drives; i++) {
+        if (strcmp(path, g_resource_drive_info[i].name) == 0) {
+            // Put this resource path into this resource drive's tree.
+            ResourceTree *tree = &g_resource_drive_info[i].tree;
+        }
+    }
+    fprintf(stderr, ERROR_ALERT "Attempted to add resource path with invalid drive, \"%s\".\n", path);
+    exit(EXIT_FAILURE);
+}
+
+static ResourceID find_resource(ResourceType type, char *path)
+{
+    /* Parse the path to find a resource if it is loaded. If it is not loaded,
+     * returns NULL.
+     */
+    // Lookup resource
+    //================================================================================
+
+    char *p = path;
+    while (1) {
+        char *end = strchr(p, '/');
+        if (end == NULL) break;
+
+        
+
+        p = end + 1;
+    }
+    // resource name
+
+
+    //================================================================================
+    return NULL_RESOURCE_ID;
 }
 
 // "create" resource. If this path is already loaded, returns the ID of the resource already in memory.
@@ -77,17 +140,47 @@ static void *get_resource(ResourceID id)
 // the first thing probably triggered a resource load, the things using the same resource are just going to recheck the path and update their ID copies.
 static ResourceID create_resource(ResourceType type, char *path)
 {
-    ResourceID id = create_resource_id(type);
-    g_resource_table[id.map_index].resource = calloc(1, g_resource_type_info[type].size);
-    mem_check(g_resource_table[id.map_index]);
+    ResourceID id = find_resource(type, path);
+    if (id != NULL_RESOURCE_ID) {
+        // Resource is loaded. Return the resource ID, but caller doesn't know it isn't "creating" it.
+        return id;
+    }
+    // Load the resource
+    //================================================================================
+    // This resource isn't loaded. Actually create the resource.
+    id = create_resource_id(type, path);
+    g_resource_table[id.table_index].uuid = id.uuid;
+    g_resource_table[id.table_index].type = type;
+    // Allocate and zero-initialize the memory needed for this type of resource, and give to the new resource entry.
+    g_resource_table[id.table_index].resource = calloc(1, g_resource_type_info[type].size);
+    mem_check(g_resource_table[id.table_index].resource);
+    // Store the path on the heap and give to the new resource entry.
+    g_resource_table[id.table_index].path = (char *) malloc((strlen(path) + 1) * sizeof(char));
+    mem_check(g_resource_table[id.table_index].path);
+    strcpy(g_resource_table[id.table_index].path, path);
+
+    // ...
+    //================================================================================
+    
+    return id;
 }
 
-#define MAX_SHADER_NAME_LENGTH 32;
-typedef struct Shader_s {
-    ShaderID id;
-    char *hash_path;
-    char *resource_path;
-    GLuint vram_id;
-} Shader;
-
+// Similar to realloc, the use of this function must reset the identifier, since it may be moved.
+// void *resource;
+// self->thing = get_resource(&resource, self->thing);
+static ResourceID get_resource(void **resource, ResourceID id)
+{
+    // Table lookup the resource.
+    //================================================================================
+    if (g_resource_table[id.table_index].uuid == id.uuid) {
+        // The point of this. Resource loading and unloading should be very rare compared to references to the resource,
+        // so that should be a constant fast lookup, yet still trigger a resource load if needed, unknown to the caller.
+        *resource = g_resource_table[id.table_index].resource;
+        return id;
+    }
+    //================================================================================
+    // If the table lookup didn't work, the resource may still be loaded somewhere else. This either gets that through
+    // the path lookup, or if that fails, loads the resource.
+    return create_resource(id.type, id.path);
+}
 

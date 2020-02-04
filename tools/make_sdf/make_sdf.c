9fc13e16621b0e2f42102530dc8d3547bccb2f8e
lucaspayne <lucaspayyne@gmail.com>
2020-02-04 16:51:04 +1300

    sdf

--- tools/make_sdf/make_sdf.c
+++ tools/make_sdf/make_sdf.c
@@ -14,6 +14,8 @@ https://www.freetype.org/freetype2/docs/tutorial/step1.html
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <math.h>
+#include <stdbool.h>
 
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define MAX(X,Y) ((X) < (Y) ? (Y) : (X))
@@ -92,27 +94,84 @@ void make_sdf(char *font_filename, int size_x, int size_y, int reach)
         }
         int sdf_width = glyph_dimensions[2*('z' - 'A' + c) + 0];
         int sdf_height = glyph_dimensions[2*('z' - 'A' + c) + 1];
+        printf("Computing sdf for glyph \'%c\' ...\n", c);
+        printf("sdf_width: %d\nsdf_height: %d\nsuper_width: %d\nsuper_height: %d\n", sdf_width, sdf_height, super_width, super_height);
+    
+        // Prepare the PNG image to be written.
+        // Prepare the memory to store the sdf image, in a form which can be passed to libpng for the creation of a PNG file.
+        const int buf_size = 4096;
+        char buf[buf_size];
+        snprintf(buf, buf_size, "char_%c.png", c);
+        FILE *glyph_png = fopen(buf, "wb");
+        if (glyph_png == NULL) {
+            printf("Failed to create PNG file for glyph \'%c\'\n.", c);
+            exit(1);
+        }
+        // https://www.lemoda.net/c/write-png/
+#define libpng_error(STR) { fprintf(stderr, "libpng error: " STR "\n"); exit(1); }
+        png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        if (png_ptr == NULL) libpng_error("Failed to create a PNG struct.");
+        png_infop info_ptr = png_create_info_struct(png_ptr);
+        if (info_ptr == NULL) libpng_error("Failed to create PNG info struct.");
+        // <setjmp.h>
+        // $man setjmp
+        if (setjmp(png_jmpbuf(png_ptr))) libpng_error("setjmp failed.");
+        png_set_IHDR(png_ptr, info_ptr, sdf_width, sdf_height, 8, // depth
+                     PNG_COLOR_TYPE_GRAY,
+                     PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_DEFAULT,
+                     PNG_FILTER_TYPE_DEFAULT);
+        png_byte **sdf_png_rows = png_malloc(png_ptr, sizeof(png_byte *) * sdf_height);
+        mem_check(sdf_png_rows);
+        for (int i = 0; i < sdf_height; i++) {
+            png_byte *sdf_png_row = png_malloc(png_ptr, sizeof(png_byte) * sdf_width);
+            mem_check(sdf_png_row);
+            sdf_png_rows[i] = sdf_png_row;
+        }
+
         for (int sdf_i = 0; sdf_i < sdf_width; sdf_i++) {
             for (int sdf_j = 0; sdf_j < sdf_height; sdf_j++) {
-                int super_i = sdf_i / sdf_supersample;
-                int super_j = sdf_j / sdf_supersample;
+                int super_i = sdf_i * sdf_supersample;
+                int super_j = sdf_j * sdf_supersample;
                 // check against a square determined by the value of reach.
-                unsigned int min_square_dist = -1;
+                unsigned int min_square_dist = reach * reach;
                 int tlx, tly, brx, bry;
                 tlx = MAX(0, super_i - reach);
                 tly = MAX(0, super_j - reach);
                 brx = MIN(super_width - 1, super_i + reach);
                 bry = MIN(super_height - 1, super_j + reach);
+                bool inside = super_image[super_i * super_width + super_j] != 0;
+                // To test for the distance to the border without rasterizing the glyph outline,
+                // just find the closest == 0 if the super-image is > 0, and > 0 if the super-image is == 0. 
                 for (int square_i = tlx; square_i <= brx; square_i++) {
                     for (int square_j = tly; square_j <= bry; square_j++) {
-                        if (
-                        ABS(square_i 
+                        bool square_point_inside = super_image[square_i * super_width + square_j] != 0;
+                        if (inside == square_point_inside) continue; // looking for min distance to 
+                        unsigned int square_dist = (square_i - super_i) * (square_i - super_i) + (square_j - super_j) * (square_j - super_j);
+                        if (square_dist < min_square_dist) min_square_dist = square_dist;
                     }
                 }
+                float min_dist = sqrt(min_square_dist);
+                if (!inside) min_dist = -min_dist;
+
+                // Map this floating point distance value, ranging from [-reach, reach], to [0, 255].
+                uint8_t mapped_val = (int) (((min_dist + reach) / (2 * reach)) * 256);
+                sdf_png_rows[sdf_i][sdf_j] = mapped_val;
+                // printf("%.2f\n", min_dist);
+                // getchar();
             }
         }
-        
+        // Save the PNG to the file.
+        png_init_io(png_ptr, glyph_png);
+        png_set_rows(png_ptr, info_ptr, sdf_png_rows);
+        png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+        // Free resources.
         free(super_image);
+        // Free libpng resources.
+        for (int i = 0; i < sdf_height; i++) {
+            png_free(png_ptr, sdf_png_rows[i]);
+        }
+        png_free(png_ptr, sdf_png_rows);
     }
     
 
